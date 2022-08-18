75fec62e86b38d2b0ee4f9cdd408981a6c660b55
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-18 08:51:46 +0600

    fetch url

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // getelement by id 
 const getElement = (id) => {
-    return document.getElementById(id);
+    // document.getElementById(id).value;------logical error
+    return document.getElementById(id).value;
 }
 // search handle button 
 const searchHandle = () => {
-    const inputText = getElement('input');
-    const input = inputText.value;
-    console.log(input)
+    const input = getElement('input');
+    const url = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${input}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
